b7c6a90f29f66e54af25a92e3f29077c0b79d57b
Malinowsk <juanrago2015@gmail.com>
2023-02-19 15:24:37 -0300

    program2

---
+++ Practicas/Otro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Otro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Platzi!");
+
+            int height = 170;
+            int age = 22;
+            string name = "Abdel Mejia";
+            string information = $"The requested info is:\nUser name: {name} \nUser age: {age} \nUser height: {height}";
+            Console.WriteLine(information);
+        }
+    }
+}
+
