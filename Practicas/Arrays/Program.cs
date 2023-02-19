7bdbb96dc7ee1a59f68fac16bb60db64ea08bac6
Malinowsk <juanrago2015@gmail.com>
2023-02-19 18:13:20 -0300

    Programas Basicos

--- Practicas/Arrays/Program.cs
+++ Practicas/Arrays/Program.cs
@@ -11,11 +11,15 @@ namespace acavacualquiercosa
             float[] valorDeCafe;
 
             tiposDeCafe = new string[] { "Simple", "Cortado" , "Expresso" };
-            valorDeCafe = new float[] { 2.0f , 10.5f , 25.5f };
+            valorDeCafe = new float[] { 4.0f , 10.5f , 25.5f };
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < tiposDeCafe.Length; i++)
             {
+
                 Console.WriteLine( "Cafe: " + tiposDeCafe[i] + " , Valor : $ " + valorDeCafe[i]);
+                
+                // Otra forma mas moderna que se agrego con la feature C# 6
+                //Console.WriteLine($"Cafe: {tiposDeCafe[i]}  , Valor : $  {valorDeCafe[i]}");
             }
         }
     }
